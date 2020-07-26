9126c485f8e021eb058267a950c040a7d4207458
arinzeJeffrey <nnamenearinze@gmail.com>
2020-07-27 00:24:41 +0100

    added a few more components and added the scss style cheats

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -8,7 +8,9 @@ const CollectionPreview = ({title, items}) => {
             <h1 className="title">{title.toUpperCase()}</h1>
             <div className="preview">
                 {
-                    items.filter((item, idx) => idx < 4).map(item =>(
+                    items
+                    .filter((item, idx) => idx < 4)
+                    .map(item =>(
                         <div key={item.id}>{item.name}</div>
                     ))
                 }
