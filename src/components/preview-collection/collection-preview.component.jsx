389fcd40a19b84e54f1b81e9e4665fe859ccff5f
arinzeJeffrey <nnamenearinze@gmail.com>
2020-07-27 00:34:34 +0100

    shop page almost done

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./collection-preview.styles.scss"
+import CollectionItem from '../collection-item/collection-item.component';
 
 
 const CollectionPreview = ({title, items}) => {
@@ -10,8 +11,8 @@ const CollectionPreview = ({title, items}) => {
                 {
                     items
                     .filter((item, idx) => idx < 4)
-                    .map(item =>(
-                        <div key={item.id}>{item.name}</div>
+                    .map(({id, ...otherItemProps}) =>(
+                        <CollectionItem key={id} {...otherItemProps}/>
                     ))
                 }
             </div>
