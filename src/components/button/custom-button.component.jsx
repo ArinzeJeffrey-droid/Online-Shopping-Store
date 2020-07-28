f57674a23f935b5d3bcc254204c0521d2f3bdfd0
arinzeJeffrey <nnamenearinze@gmail.com>
2020-07-28 01:53:46 +0100

    created the button component and its styles file

---
+++ src/components/button/custom-button.component.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import "./custom-button.styles.scss"
+
+const CustomButton = ({children, ...otherProps}) => {
+    return (
+        <div className="custom-button" {...otherProps}>
+
+        </div>
+    )
+}
