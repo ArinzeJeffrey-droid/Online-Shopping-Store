b9cc8649a5e38667cfb573a6179af19bdeafd2e5
arinzeJeffrey <nnamenearinze@gmail.com>
2020-07-30 23:46:44 +0100

    installed the redux package and started work on the user redux file

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom"
+import { Provider } from "react-redux"
 
 ReactDOM.render(
   <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider>
+    <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
