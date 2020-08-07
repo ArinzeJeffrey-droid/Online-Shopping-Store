78d0cc1bb95d79f189acd302ae4f7cdc2ee271f6
arinzeJeffrey <nnamenearinze@gmail.com>
2020-08-07 01:26:29 +0100

    pass the required props to the checkout component

--- src/pages/checkout-page/checkout.component.jsx
+++ src/pages/checkout-page/checkout.component.jsx
@@ -1,8 +1,11 @@
 import React from "react"
+import { connect } from "react-redux"
+import {createStructuredSelector} from "reselect"
+import {selectCartItems, selectCartTotal} from "../../redux/cart/cart.selectors"
 import "./checkout.styles.scss"
 
 
-const CheckoutPage = () => {
+const CheckoutPage = ({cartItems, total}) => {
     return (
         <div className="checkout-page">
             <div className="checkout-header">
@@ -22,8 +25,20 @@ const CheckoutPage = () => {
                     <span>Remove</span>
                 </div>
             </div>
+            {
+                cartItems.map(cartItem =>
+                    cartItem.name
+                )
+            }
+            <div className="total">
+                <span>TOTAL: ${total}</span>
+            </div>
         </div>
     );
 }
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems,
+    total: selectCartTotal
+})
 
-export default CheckoutPage;
+export default connect(mapStateToProps)(CheckoutPage);
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -24,3 +24,12 @@ export const selectCartItemsCount = createSelector(
         0
     )
 )
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems =>
+    cartItems.reduce(
+        (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity * cartItem.price,
+        0
+    )
+)
