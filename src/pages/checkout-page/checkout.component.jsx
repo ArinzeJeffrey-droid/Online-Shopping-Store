aa63ad6d25b67d6063385f028708fa5e0b9f3424
arinzeJeffrey <nnamenearinze@gmail.com>
2020-08-07 01:15:24 +0100

    base styles for the checkout page added

--- src/pages/checkout-page/checkout.component.jsx
+++ src/pages/checkout-page/checkout.component.jsx
@@ -4,8 +4,24 @@ import "./checkout.styles.scss"
 
 const CheckoutPage = () => {
     return (
-        <div>
-            Checkout Page
+        <div className="checkout-page">
+            <div className="checkout-header">
+                <div className="header-block">
+                    <span>Product</span>
+                </div>
+                <div className="header-block">
+                    <span>Description</span>
+                </div>
+                <div className="header-block">
+                    <span>Quantity</span>
+                </div>
+                <div className="header-block">
+                    <span>Price</span>
+                </div>
+                <div className="header-block">
+                    <span>Remove</span>
+                </div>
+            </div>
         </div>
     );
 }
