f77c13ae67e10346cba77c9da6d33d9c96555353
arinzeJeffrey <nnamenearinze@gmail.com>
2020-08-18 22:52:25 +0100

    initialized the shop reducer function

--- src/pages/shop-page/shop.component.jsx
+++ src/pages/shop-page/shop.component.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import SHOP_DATA from "./shop.data.js"
 import CollectionPreview from '../../components/preview-collection/collection-preview.component.jsx';
+import { selectCollections } from "../../redux/shop/shop.selector"
+import { connect } from "react-redux"
+import { createStructuredSelector } from "reselect"
 
 
-class ShopPage extends React.Component {
-    constructor(){
-        super()
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-    render() {
-        const {collections} = this.state
+const ShopPage = ({ collections }) => {
         return (
             <div className="shop-page">
                 {
@@ -21,7 +15,10 @@ class ShopPage extends React.Component {
                 }
             </div>
         );
-    }
-}
 
-export default ShopPage;
+}
+const mapStateToProps =  createStructuredSelector({
+    collections: selectCollections
+})
+
+export default connect(mapStateToProps)(ShopPage);
