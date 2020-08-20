0ab8df737c2a71678f47694854fa5f5709366423
arinzeJeffrey <nnamenearinze@gmail.com>
2020-08-20 22:05:03 +0100

    implemented dynamic url routing with react-router-dom

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { connect } from "react-redux"
+import { selectCollection } from "../../redux/shop/shop.selector"
 import CollectionsItem from "../../components/collection-item/collection-item.component"
-import "collection.styles.scss"
+import "./collection.styles.scss"
 
 
 
-const CollectionPage = ({ match }) => {
+const CollectionPage = ({ collection }) => {
+    console.log(collection)
     return (
         <div className="collection-page">
             <h2>CATEGORY PAGE</h2>
         </div>
     ); 
 }
-
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+export default connect(mapStateToProps)(CollectionPage);
 
 
 
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,5 +1,21 @@
 import { createSelector } from "reselect"
 
+const COLLECTION_ID_MAP = {
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    womens: 4,
+    mens: 5
+}
+
+export const selectCollection = collectionUrlParam => {
+    createSelector(
+        [selectCollections],
+        collections => collections.find(
+            collections.id === COLLECTION_ID_MAP[collectionUrlParam]
+        )
+    )
+}
 
 const selectShop = state => state.shop
 
