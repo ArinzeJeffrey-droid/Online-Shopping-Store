990fab4cb71768aae3ad83f2bd3bbfc5e193b506
arinzeJeffrey <nnamenearinze@gmail.com>
2020-08-28 21:50:39 +0100

    changed the shoppage component to a class component

--- src/pages/shop-page/shop.component.jsx
+++ src/pages/shop-page/shop.component.jsx
@@ -4,14 +4,19 @@ import CollectionsOverview from "../../components/collections-overview/collectio
 import CollectionPage from '../collection/collection.component';
 
 
-const ShopPage = ({ match }) => {
+class ShopPage extends React.Component {
+    constructor(){
+        super()
+    }
+    render(){
+        const { match } = this.props
         return (
             <div className="shop-page">
                 <Route exact path={`${match.path}`} component={CollectionsOverview}/>
                 <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
-
             </div>
         );
+    }
 
 }
 
