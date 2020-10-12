4eb1f48eda536ba4845c3fb59d7dad958f421857
arinzeJeffrey <nnamenearinze@gmail.com>
2020-10-12 12:23:22 +0100

    added the redux saga library to replace redux thunk

--- src/pages/shop-page/shop.component.jsx
+++ src/pages/shop-page/shop.component.jsx
@@ -4,8 +4,8 @@ import { createStructuredSelector } from "reselect"
 import { connect } from "react-redux"
 import CollectionsOverview from "../../components/collections-overview/collection-overview.component"
 import CollectionPage from '../collection/collection.component';
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
-import { selectIsCollectionFetching } from "../../redux/shop/shop.selector"
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+import { selectIsCollectionFetching, selectIsCollectionsLoaded } from "../../redux/shop/shop.selector"
 import withSpinner from '../../components/with-spinner/with-spinner.component';
 
 const CollectionsOverviewWithSpinner = withSpinner(CollectionsOverview)
@@ -13,26 +13,27 @@ const CollectionPageWithSpinner = withSpinner(CollectionPage)
 
 class ShopPage extends React.Component {
     componentDidMount(){ 
-        const { fetchCollectionsStartAsync } = this.props
-        fetchCollectionsStartAsync()
+        const { fetchCollectionsStart } = this.props
+        fetchCollectionsStart()
     }
     render(){
-        const { match, isCollectionFetching } = this.props
+        const { match, isCollectionsLoaded } = this.props
         return (
             <div className="shop-page">
-                <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={isCollectionFetching} {...props} />}/>
-                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={isCollectionFetching} {...props} />}/>
+                <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={!isCollectionsLoaded} {...props} />}/>
+                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props} />}/>
             </div>
         );
     }
 
 }
 const mapStateToProps = createStructuredSelector({
-    isCollectionFetching: selectIsCollectionFetching
+    isCollectionFetching: selectIsCollectionFetching,
+    isCollectionsLoaded: selectIsCollectionsLoaded
 })
 
 const mapDispatchToProps = dispatch => ({
-    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 })
 
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,19 @@
 import { createStore, applyMiddleware } from "redux"
 import { persistStore } from "redux-persist"
 import logger from "redux-logger"
-import thunk from "redux-thunk"
+import createSageMiddleware from "redux-saga"
+import { fetchCollectionsStart } from "./shop/shop.saga"
 
 import rootReducer from "./root-reducer"
+const sagaMiddleware = createSageMiddleware()
 
-const middlewares = [thunk]
+const middlewares = [sagaMiddleware]
 if (process.env.NODE_ENV === "development") {
     middlewares.push(logger)
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+sagaMiddleware.run(fetchCollectionsStart)
 
 export const persistor = persistStore(store)
 
